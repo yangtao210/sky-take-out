6e99fe3a2b00a591ba67930494d2a77d967e9f7e
yangtaoyangtao210 <1973705434@qq.com>
2024-11-20 12:58:05 +0800

    图片上传1

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -1,4 +1,14 @@
 package com.sky.controller.admin;
 
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 菜品管理
+ */
+@RestController
+@RequestMapping("/admin/dish")
+@Api(tags = "菜品管理")
 public class DishController {
 }
